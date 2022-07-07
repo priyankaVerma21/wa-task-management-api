799d053831cbb1ee703e4e13bbe82044f1e6d8f4
Dharmendra Kumar <dharmendra.kumar@hmcts.net>
2022-07-07 10:04:35 +0100

    add assignee file generation

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/watasks/controllers/UpdateCamundaTaskVariable.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskmanagementapi/watasks/controllers/UpdateCamundaTaskVariable.java
@@ -12,13 +12,17 @@ import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.camunda.HistoricC
 import uk.gov.hmcts.reform.wataskmanagementapi.domain.entities.camunda.TaskState;
 import uk.gov.hmcts.reform.wataskmanagementapi.services.CamundaTaskUpdateService;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 public class UpdateCamundaTaskVariable extends SpringBootFunctionalBaseTest {
@@ -32,6 +36,33 @@ public class UpdateCamundaTaskVariable extends SpringBootFunctionalBaseTest {
     @Before
     public void before() {
         //
+    }
+
+    @Test
+    public void update_camunda_task_variable_while_reading_from_a_file() {
+        String authorisation = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3YV90YXNrX21hbmFnZW1lbnRfYXBpIiwiZXhwIjoxNjU2NzcwNTUzfQ.0NxOCOKcOYJ-a9q7xARN3AuIQtGYVyey83DQQG33N0-XXdwGLdCyyxhJmzLB8YiVRwd54_SjT821g0X6XFLzFg";
+        String input = getResource("assigned_task_for_migration.csv");
+        String[] split = input.replaceAll("    ", "").split("\n");
+
+
+        List<String> failedTasks = new ArrayList<>();
+        Arrays.asList(split).forEach(taskId ->
+                                     {
+                                         log.info("updating cft task state of task id " + taskId.trim());
+                                         try {
+                                             camundaService.updateCftTaskState(
+                                                 taskId.trim(),
+                                                 TaskState.UNCONFIGURED,
+                                                 authorisation
+                                             );
+                                         } catch (Exception ex) {
+                                             failedTasks.add(taskId);
+                                         }
+                                     });
+
+
+        log.info("failed tasks are ");
+        failedTasks.forEach(t -> log.info("failed task id is {}", t));
     }
 
     @Test
@@ -67,7 +98,11 @@ public class UpdateCamundaTaskVariable extends SpringBootFunctionalBaseTest {
                              String taskId = task.getId();
                              log.info("deleting cft task state of task id " + taskId);
                              try {
-                                 camundaService.updateCftTaskState(taskId, TaskState.UNCONFIGURED, authorisation);
+                                 camundaService.updateCftTaskState(
+                                     "e2a524ad-f956-11ec-be36-e66049a77c29",
+                                     TaskState.UNCONFIGURED,
+                                     authorisation
+                                 );
                              } catch (Exception ex) {
                                  failedTasks.add(taskId);
                              }
@@ -76,6 +111,58 @@ public class UpdateCamundaTaskVariable extends SpringBootFunctionalBaseTest {
 
             log.info("failed tasks are ");
             failedTasks.forEach(t -> log.info("failed task id is {}", t));
+        }
+    }
+
+    @Test
+    public void generate_query_for_cft_task_update() {
+        String authorisation = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ3YV90YXNrX21hbmFnZW1lbnRfYXBpIiwiZXhwIjoxNjU2NzcwNTUzfQ.0NxOCOKcOYJ-a9q7xARN3AuIQtGYVyey83DQQG33N0-XXdwGLdCyyxhJmzLB8YiVRwd54_SjT821g0X6XFLzFg";
+        String query = getResource("camunda-task-assigned-query.json");
+            for (int firstResult = 0; firstResult < 2461; firstResult += 100) {
+                List<CamundaTask> historicCamundaTasks = camundaService.searchTaskWithCriteria(
+                    query,
+                    firstResult,
+                    100,
+                    authorisation
+                );
+                AtomicReference<List<String>> taskIds = new AtomicReference<>(new ArrayList<>());
+                AtomicReference<String> assignee = new AtomicReference<>();
+                historicCamundaTasks
+                    .forEach(task ->
+                             {
+                                 if (assignee.get() == null || task.getAssignee().equals(assignee.get())) {
+                                     taskIds.get().add(task.getId());
+                                     //for first time
+                                     assignee.set(task.getAssignee());
+
+                                 } else {
+                                     StringBuilder output = new StringBuilder("update tasks set assignee = '");
+                                     output.append(assignee.get());
+                                     output.append("', state = 'ASSIGNED'");
+                                     output.append(" where task_id in (");
+                                     taskIds.get().forEach(id -> {
+                                         output.append("'");
+                                         output.append(id);
+                                         output.append("',");
+                                     });
+                                     output.deleteCharAt(output.length() - 1);
+                                     output.append(");");
+                                     log.info(output.toString());
+                                     taskIds.set(new ArrayList<>());
+                                     assignee.set(null);
+
+                                 }
+
+
+                             });
+            }
+    }
+
+    private void writeToFile(BufferedWriter info, StringBuilder output) {
+        try {
+            info.write(output.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -95,7 +182,7 @@ public class UpdateCamundaTaskVariable extends SpringBootFunctionalBaseTest {
                 + finishedBefore + "\",");
 
         List<String> failedTasks = new ArrayList<>();
-        for (int firstResult = 0; firstResult < 9; firstResult += 100) {
+        for (int firstResult = 0; firstResult < 100; firstResult += 100) {
             log.info("firstResult " + firstResult);
             String firstResultString = Integer.toString(firstResult);
             log.info("processing for first result " + firstResultString);
